d893b6a83b221e5953025f6db8bf6eaacea2e0fd
Luke Clewlow <luke.clewlow@gmail.com>
2014-12-03 12:53:38 +0000

    User now able to add responses

--- chatbot.rb
+++ chatbot.rb
@@ -1,11 +1,14 @@
+
+
+
 def get_response(input)
-  key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
+  key = @RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
   /#{key}/ =~ input
-  response = RESPONSES[key]
+  response = @RESPONSES[key]
   response.nil? ? 'sorry?' : response % { c1: $1, c2: $2, c3: $3}
 end
 
-RESPONSES = { 'sayonara' => 'sayonara', 
+@RESPONSES = { 'sayonara' => 'sayonara', 
               'the weather is (.*)' => 'I hate it when it\'s %{c1}', 
               'I love (.*)' => 'I love %{c1} too', 
               'I groove to (.*) and (.*)' => 'I love %{c1} but I hate %{c2}',
@@ -17,7 +20,8 @@ RESPONSES = { 'sayonara' => 'sayonara',
               'Haha' => 'Lol',
               "What's yours?" => 'Jimbob Letters, pleased to make your acquaintance',
               "My favourite animal is a (.*)" => "Nice, my favourite is also %{c1}",
-              "You heard" => "I heard, but I didn't understand"
+              "You heard" => "I heard, but I didn't understand",
+              "What day is it?" => "Your lucky day"
           }
 
 
@@ -42,11 +46,25 @@ def convo
   print "...>"
   input = gets.chomp
   while input != ( "goodbye" || "bye" ) #bye does not work, not sure why
-  puts "\e[34m++bot++ #{get_response(input)}\e[0m"
-  print "...>"
-  input = gets.chomp
+    if "#{get_response(input)}" == "sorry?"
+      puts "\e[34m++bot++ That's not in my repotoire, would you like to add a response?\e[0m"
+      puts ""
+      puts "Please enter what the bot should reply to this phrase in future?".center(50)
+      puts ""
+      print "...>"
+      new_response = gets.chomp    
+      @RESPONSES["#{input}"] = "#{new_response}"
+      puts "\e[34m++bot++ Added...\e[0m"
+      print "...>"
+      input = gets.chomp
+    else
+      puts "\e[34m++bot++ #{get_response(input)}\e[0m"
+      print "...>"
+      input = gets.chomp
+    end
   end
 end
+
 
 header
 intro
